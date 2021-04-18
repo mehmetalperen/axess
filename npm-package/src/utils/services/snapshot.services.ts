07dda7207aa6055d762c35c45e49a4935fd4b2ee
omerdemirkan <omerfarukpiano@gmail.com>
2021-04-18 09:31:06 -0700

    fix create snapshot service

--- npm-package/src/utils/services/snapshot.services.ts
+++ npm-package/src/utils/services/snapshot.services.ts
@@ -1,14 +1,19 @@
 import { Snapshot } from "../types";
-import {db} from  '../helpers/firebase.helpers'
+import { db } from "../helpers/firebase.helpers";
 
 export async function createSnapshot(snapshot: Snapshot) {
-    // send a post request to store the snapshot
     try {
-        //const res = await firebaseClient.post("/snapshots.json", snapshot);
-        //console.log(res);
-        await db.collection("snapshots").doc("1").update(snapshot)   
-         const res = await db.collection("snapshots").get()
-        console.log(res)
+        await db.collection("snapshots").add({
+            before: {
+                html: snapshot.before.src,
+            },
+            after: {
+                html: snapshot.after.src,
+            },
+            violation: snapshot.violation,
+        });
+        const res = await db.collection("snapshots").get();
+        console.log(res);
     } catch (e) {
         console.error(e);
     }
